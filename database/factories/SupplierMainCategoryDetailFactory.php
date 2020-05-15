bad4abade772c5d1520e1d33a88d02086f59b4c9
LeBron <1004578796@qq.com>
2020-05-15 17:40:36 +0800

    migration

---
+++ database/factories/SupplierMainCategoryDetailFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\SupplierMainCategoryDetail;
+use Faker\Generator as Faker;
+
+$factory->define(SupplierMainCategoryDetail::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
