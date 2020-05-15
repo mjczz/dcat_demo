299aa11707a415780271af16e4f6bb8357a901a1
LeBron <1004578796@qq.com>
2020-05-15 17:35:31 +0800

    主营类目详情

---
+++ app/SupplierMainCategoryDetail.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SupplierMainCategoryDetail extends Model
+{
+    protected $table = 't_supplier_main_category_detail';
+
+    public $timestamps = false;
+}
