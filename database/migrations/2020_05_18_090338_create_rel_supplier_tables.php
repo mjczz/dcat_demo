7eae2270521e73a23f6ab07b8453c921b6cd1898
LeBron <1004578796@qq.com>
2020-05-25 17:54:42 +0800

    哈哈哈，今天是星期一

--- database/migrations/2020_05_18_090338_create_rel_supplier_tables.php
+++ database/migrations/2020_05_18_090338_create_rel_supplier_tables.php
@@ -119,9 +119,12 @@ class CreateRelSupplierTables extends Migration
             $table->increments('id');
             $table->integer("supplier_id")->comment("供货商id");
             $table->string('ship_info', 100)->default('')->comment('发货信息');
-            $table->string('counterpart', 50)->default('')->comment('对接人新买');
+            $table->string('counterpart', 50)->default('')->comment('对接人姓名');
             $table->string('mobile', 50)->default('')->comment('对接人号码');
-            $table->integer("ship_area_id")->comment("发货地区ID");
+            $table->integer("area_id_1")->default(0)->comment("省id");
+            $table->integer("area_id_2")->default(0)->comment("市id");
+            $table->integer("area_id_3")->default(0)->comment("区id");
+            $table->integer("ship_area_id")->default(0)->comment("发货地区id，有可能是市id有可能是区id");
             $table->string('ship_address', 50)->default('')->comment('发货详细地址');
             $table->integer('ctime')->default(0);
             $table->integer('utime')->default(0);
