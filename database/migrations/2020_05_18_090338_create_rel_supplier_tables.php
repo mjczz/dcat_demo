8a30941b9d3ba6c4a7f15c2c82035567f6a3ab79
LeBron <1004578796@qq.com>
2020-06-09 15:07:34 +0800

    哈哈哈，今天是星期二

--- database/migrations/2020_05_18_090338_create_rel_supplier_tables.php
+++ database/migrations/2020_05_18_090338_create_rel_supplier_tables.php
@@ -77,7 +77,7 @@ class CreateRelSupplierTables extends Migration
             $table->integer('success_shenhe_time')->default(0)->comment("审核通过时间");
             $table->string("refuse_reason")->default('')->comment("审核拒绝理由");
             $table->integer('onsale_goods_num')->default('0')->comment('在架商品数');
-            $table->string("shenhe_material", 1000)->default('')->comment("审核材料");
+            $table->string("shenhe_material", 2000)->default('')->comment("审核材料");
             $table->integer('ctime')->default(0);
             $table->integer('utime')->default(0);
             $table->integer('cuid')->default(0);
@@ -94,11 +94,11 @@ class CreateRelSupplierTables extends Migration
             $table->integer("t_brand_id")->comment("关联t_brand id");
             $table->string('name', 50)->default('')->comment('品牌名');
             $table->tinyInteger("brand_is_zhuan")->default(2)->comment("品牌是否是其他公司转让：1是 2否");
-            $table->string('trademark_registration', 1000)->default('')->comment('商标注册证明');
-            $table->string('trademark_zhuan', 1000)->default('')->comment('商标转让证明');
-            $table->string('production_license', 1000)->default('')->comment('生产许可证');
-            $table->string('business_license', 1000)->default('')->comment('营业执照');
-            $table->string('authorization_letter', 1000)->default('')->comment('授权书');
+            $table->string('trademark_registration', 2000)->default('')->comment('商标注册证明');
+            $table->string('trademark_zhuan', 2000)->default('')->comment('商标转让证明');
+            $table->string('production_license', 2000)->default('')->comment('生产许可证');
+            $table->string('business_license', 2000)->default('')->comment('营业执照');
+            $table->string('authorization_letter', 2000)->default('')->comment('授权书');
             $table->string('changjia', 2000)->default('')->comment('生产厂家资料');
             $table->tinyInteger("shenhe_status")->default(1)->comment("审核状态：1审核中 2审核通过 3审核拒绝");
             // 2个更新方式：1入驻时的提审时间 2入驻后自己添加品牌时的提交审核时间,提交审核后，要更新t_brand
